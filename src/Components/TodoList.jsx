c2ed74c88d1221952d2a36e8773edee2ac0ab3b6
Sayed <sayedshehata33@gmail.com>
2024-07-03 22:09:48 +0300

    feat: :building_construction: Make structural changes

---
+++ src/Components/TodoList.jsx
@@ -0,0 +1,13 @@
+import AddTodo from "./AddTodo";
+import TodoItem from "./TodoItem";
+
+const TodoList = () => {
+  return (
+    <div>
+      <AddTodo />
+      <TodoItem />
+    </div>
+  );
+};
+
+export default TodoList;
