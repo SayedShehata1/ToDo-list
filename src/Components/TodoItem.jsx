d0b0c9ae3ac8ea5e5d1f0bd38c6f2922fd1caa30
Sayed <sayedshehata33@gmail.com>
2024-07-03 22:49:52 +0300

    feat: :sparkles: handle TodoItem and TodoList

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -1,5 +1,27 @@
-const TodoItem = () => {
-  return <div>TodoItem</div>;
+const TodoItem = ({ task, toggleCompletedTask, removeTask }) => {
+  const handleToggleCompletion = () => {
+    toggleCompletedTask(task.id);
+  };
+
+  const handleRemoveTask = () => {
+    removeTask(task.id);
+  };
+
+  return (
+    <div style={{ marginBottom: "10px" }}>
+      <input
+        type="checkbox"
+        checked={task.completed}
+        onChange={handleToggleCompletion}
+      />
+      <span
+        style={{ textDecoration: task.completed ? "line-through" : "none" }}
+      >
+        {task.text}
+      </span>
+      <button onClick={handleRemoveTask}>Remove</button>
+    </div>
+  );
 };
 
 export default TodoItem;
--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,11 +1,46 @@
 import AddTodo from "./AddTodo";
 import TodoItem from "./TodoItem";
+import { fetchTasks, updateTask, deleteTask } from "../../API/api";
+import { useEffect, useState } from "react";
 
 const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // Fetch tasks from the API
+    const getTasks = async () => {
+      const apiTasks = await fetchTasks();
+      setTasks(apiTasks);
+    };
+    getTasks();
+  }, []);
+
+  // Toggle Completed Task function
+  const HandleToggleCompleteTask = async (taskId) => {
+    const taskToToggle = tasks.find((task) => task.id === taskId);
+    const updatedTask = { ...taskToToggle, completed: !taskToToggle.completed };
+    const savedTask = await updateTask(taskId, updatedTask);
+    setTasks(tasks.map((task) => (task.id === taskId ? savedTask : task)));
+  };
+
+  // Remove Task function
+  const handleRemoveTask = async (taskId) => {
+    await deleteTask(taskId);
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
   return (
     <div>
+      <h2>Todo List</h2>
       <AddTodo />
-      <TodoItem />
+      {tasks.map((task) => (
+        <TodoItem
+          key={task.id}
+          task={task}
+          toggleCompletedTask={HandleToggleCompleteTask}
+          removeTask={handleRemoveTask}
+        />
+      ))}
     </div>
   );
 };
